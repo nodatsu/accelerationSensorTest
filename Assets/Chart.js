b79bd555cba71eda13bab9348d87cc3137b80671
角田 均 <tsunoda@aomori-u.ac.jp>
2015-08-20 15:54:06 +0900

    sensor value + chart

---
+++ Assets/Chart.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var lineRenderer: LineRenderer;
+var position: Vector3;
+var next_position: Vector3;
+var gravity: Vector3;
+var acceleration: Vector3;
+
+function Start() {
+	lineRenderer = GetComponent(LineRenderer);
+	lineRenderer.enabled = true;
+	lineRenderer.SetVertexCount(2);
+	position = transform.position;
+	next_position = position;
+
+	lineRenderer.SetPosition(0, position);
+}
+
+function Update () {
+	var alpha = 0.8;
+	gravity = alpha * gravity + (1 - alpha) * Input.acceleration;
+
+  	var lenear_acceleration = Input.acceleration - gravity;
+  	
+  	acceleration = alpha * acceleration + (1 - alpha) * lenear_acceleration;
+
+	next_position.x = position.x + acceleration.x * 10;
+	next_position.y = position.y + acceleration.y * 10;
+	lineRenderer.SetPosition(1, next_position);
+}
---
+++ Assets/SensorValue.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start() {
+	gameObject.GetComponent(UI.Text).text = "test";
+}
+
+function Update () {
+	var str: String;
+	
+	str = "X: " + Input.acceleration.x + "\nY: " + Input.acceleration.y
+		+ "\nZ: " + Input.acceleration.z;
+	
+	gameObject.GetComponent(UI.Text).text = str;
+}
