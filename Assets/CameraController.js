f72464bb53809fa848c9bb518e46f567e0615ea0
角田 均 <tsunoda@aomori-u.ac.jp>
2015-08-20 22:40:03 +0900

    skybox, gyro, UnityChan

--- Assets/CameraController.js
+++ Assets/CameraController.js
@@ -1,5 +1,52 @@
 ﻿#pragma strict
 
+var gravity: Vector3;
+var acceleration: Vector3;
+
 function Awake () {
-        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	Screen.sleepTimeout = SleepTimeout.NeverSleep;
+}
+
+function Start () {
+	if (Application.platform == RuntimePlatform.Android) {
+		Input.gyro.enabled = true;
+	}
+}
+
+function Update () {
+	if (Application.platform == RuntimePlatform.Android) {
+		var gyro: Quaternion = Input.gyro.attitude;
+		this.transform.localRotation = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyro.x, -gyro.y, gyro.z, gyro.w));
+	}
+	else if (Application.platform == RuntimePlatform.OSXEditor) {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			this.transform.Rotate(Vector3.right, -1);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			this.transform.Rotate(Vector3.right, 1);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			this.transform.Rotate(Vector3.up, -1, Space.World);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			this.transform.Rotate(Vector3.up, 1, Space.World);
+		}
+		if (Input.GetKey(KeyCode.Space)) {
+			this.transform.rotation = Quaternion.Euler(0, 0, 0);
+		}
+	}
+	
+	var alpha = 0.8;
+	gravity = alpha * gravity + (1 - alpha) * Input.acceleration;
+
+  	var linear_acceleration = Input.acceleration - gravity;
+  	
+  	acceleration = alpha * acceleration + (1 - alpha) * linear_acceleration;
+
+	GetComponent(Rigidbody).AddForce(acceleration.x * 100, acceleration.y * 100, acceleration.z * -100, ForceMode.Acceleration);
+}
+
+function Reset() {
+	this.transform.rotation = Quaternion.Euler(30, 0, 0);
+	this.transform.position = Vector3(0, 2, -2);
 }
